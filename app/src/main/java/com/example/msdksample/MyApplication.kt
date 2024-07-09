8b9e19b176a8e5cafbbf188c463ace0549b51bcf
superbabii <nzrbabii@gmail.com>
2024-07-09 07:59:24 +0300

    init

---
+++ app/src/main/java/com/example/msdksample/MyApplication.kt
@@ -0,0 +1,52 @@
+package com.example.msdksample
+
+import android.app.Application
+import android.content.Context
+import android.util.Log
+import dji.v5.common.error.IDJIError
+import dji.v5.common.register.DJISDKInitEvent
+import dji.v5.manager.SDKManager
+import dji.v5.manager.interfaces.SDKManagerCallback
+
+class MyApplication : Application() {
+
+    private val TAG = this::class.simpleName
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        // Before calling install, do not call any MSDK related
+        com.secneo.sdk.Helper.install(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        // Initialize MSDK. It is recommended to put the initialization logic in Application. Of course, you can put it anywhere according to your needs.
+        SDKManager.getInstance().init(this,object:SDKManagerCallback{
+            override fun onInitProcess(event: DJISDKInitEvent?, totalProcess: Int) {
+                Log.i(TAG, "onInitProcess: ")
+                if (event == DJISDKInitEvent.INITIALIZE_COMPLETE) {
+                    SDKManager.getInstance().registerApp()
+                }
+            }
+            override fun onRegisterSuccess() {
+                Log.i(TAG, "onRegisterSuccess: ")
+            }
+            override fun onRegisterFailure(error: IDJIError?) {
+                Log.i(TAG, "onRegisterFailure: ")
+            }
+            override fun onProductConnect(productId: Int) {
+                Log.i(TAG, "onProductConnect: ")
+            }
+            override fun onProductDisconnect(productId: Int) {
+                Log.i(TAG, "onProductDisconnect: ")
+            }
+            override fun onProductChanged(productId: Int)
+            {
+                Log.i(TAG, "onProductChanged: ")
+            }
+            override fun onDatabaseDownloadProgress(current: Long, total: Long) {
+                Log.i(TAG, "onDatabaseDownloadProgress: ${current/total}")
+            }
+        })
+    }
+}
